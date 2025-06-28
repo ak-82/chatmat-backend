daeff2df1f81b2644df31a46c78876f604c8438c
karami <alikarami@mondfx.com>
2025-06-28 14:43:20 +0330

    bugfix

--- createUsers.js
+++ createUsers.js
@@ -6,8 +6,8 @@ mongoose.connect("mongodb://localhost:27017/chatdb").then(async () => {
   await User.deleteMany({});
 
   const users = [
-    { username: "user1", password: await bcrypt.hash("83123821", 10), showName : "Wade💖" },
-    { username: "user2", password: await bcrypt.hash("86000", 10), showName : "❤️‍🔥Ember❤️‍🔥" },
+    { username: "user1", password: await bcrypt.hash("pass1", 10), showName : "Wade💖" },
+    { username: "user2", password: await bcrypt.hash("pass2", 10), showName : "❤️‍🔥Ember❤️‍🔥" },
   ];
 
   await User.insertMany(users);
